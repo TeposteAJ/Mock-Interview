459ec0c247d944d500df03bef0abe863974d3890
TeposteAJ <ada_hack184@hackademy.mx>
2023-01-07 19:41:05 -0800

    comentarios models

--- Pokemon/PokemonProyect/App/models.py
+++ Pokemon/PokemonProyect/App/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#Classe tipo modelo
 class TipoModel(models.Model):
     type = models.CharField(max_length=50)
-
+    # Solo tiene un campo este modelo que será el tipo de pokemon
+    
     class Meta:
         db_table = 'Tipo Pokemon'
-    
+         # esto nos permite configurar el nombre con el que aparecer este modelo en la BBD
 
 
 class PokemonModel(models.Model):
