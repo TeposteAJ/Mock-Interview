2bc30a7c1314ed2e60156349701aa0225dc124c5
TeposteAJ <ada_hack184@hackademy.mx>
2023-01-07 19:53:08 -0800

    Update serializer.py

--- Pokemon/PokemonProyect/App/serializer.py
+++ Pokemon/PokemonProyect/App/serializer.py
@@ -1,8 +1,13 @@
+#Primero se importan las librerias que nos permitiran crear clases que tengan las 
+#propiedades de los serializadores 
+#desde , por supesto el rest_Framework 
+#¿Qué es un rest framework?
 from rest_framework import serializers
-
+#Además importamos del archivo donde se encuentran los modelos, pues los mismos modelos que creamos antes
 from .models import (PokemonModel, TipoModel) 
 
-
+#Ahora nuestra clase será de tipo serializador
+#¿Qué es un serializador? 
 class TipoSerializer(serializers.ModelSerializer): 
     class Meta: 
         model= TipoModel
